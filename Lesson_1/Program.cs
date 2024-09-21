2d97debe9697175526fbd6d0d7cbe831b4431a82
HellSensation <hellsensation@gmail.com>
2024-09-21 19:03:05 +0400

    Добавлен вывод ФИО

--- Lesson_1/Program.cs
+++ Lesson_1/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Введите имя:");
+        string? name = Console.ReadLine();
+        
+        Console.WriteLine("Введите фамилию:");
+        string? surename = Console.ReadLine();
+        
+        Console.WriteLine("Введите отчество:");
+        string? patronymic = Console.ReadLine();
+        
+        Console.WriteLine($@"{name} {surename} {patronymic}");
     }
 }
--- Lesson_1/obj/Debug/net8.0/Lesson_1.AssemblyInfo.cs
+++ Lesson_1/obj/Debug/net8.0/Lesson_1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lesson_1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc00f82fcf382bd0995733b5f0cf03602683c069")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lesson_1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lesson_1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
