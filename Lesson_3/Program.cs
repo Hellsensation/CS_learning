6dbb6f46c4d9236db2778989197a3a9efe9a88e9
HellSensation <hellsensation@gmail.com>
2024-09-29 19:49:41 +0400

    Реализован запрос чисел и расширение массива при переполнении.

---
+++ Lesson_3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading.Channels;
+
+namespace Lesson_3;
+
+class Program
+{
+    
+    
+    static void Main(string[] args)
+    {
+        int arrLength = 1;
+        int[] numbers = new int[arrLength];
+        string number = "";
+        int iterationCount = 0;
+        
+        while (number != "q")
+        {
+            try
+            {
+                Console.WriteLine("Enter number");
+                number = Console.ReadLine();
+                numbers[iterationCount] = int.Parse(number);
+                iterationCount++;
+            }
+            catch (IndexOutOfRangeException)
+            {
+                numbers = ResizeArr(numbers);
+                numbers[iterationCount] = int.Parse(number);
+                iterationCount++;
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Введеный данные нверны. Введите число.");
+            }
+        }
+        foreach (var VARIABLE in numbers)
+        {
+            Console.WriteLine(VARIABLE);
+        }
+        
+        
+        static int[] ResizeArr(int[] nums)
+        {
+            int newArrLength = nums.Length + 1;
+            int[] newArr = new int[newArrLength];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                newArr[i] = nums[i];
+            }
+            return newArr;
+        }
+    }
+}
+
+
