fc69ac85e0dd7add226984dfd5e5c4fd5b9eac80
HellSensation <hellsensation@gmail.com>
2024-10-06 22:09:13 +0400

    initial commit

---
+++ Lesson_4/PrimeNumber.cs
@@ -0,0 +1,47 @@
+﻿namespace Lesson_4;
+
+public static class PrimeNumber
+{
+    public static void FindNumber()
+    {
+        while (true)
+        {
+            Console.Write("Введите значение N: ");
+            
+            int userNumber = int.Parse(Console.ReadLine() );
+        
+            int primeNumCount = 0; 
+            int startNum = 2;   
+
+            while (primeNumCount < userNumber)
+            {
+                if (IsPrime(startNum))
+                {
+                    primeNumCount++;
+                }
+                if (primeNumCount < userNumber) 
+                {
+                    startNum++;
+                }
+            }
+        
+            Console.WriteLine($"N-ое простое число: {startNum}");
+        }
+    }
+    
+    static bool IsPrime(int num)
+    {
+        if (num < 2)
+        {
+            return false;
+        }
+        for (int i = 2; i * i <= num; i++)
+        {
+            if (num % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ Lesson_4/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Lesson_4;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Что будем делать?\n1 - Поск простого числа\n2 - Игра с курицей");
+        string userChoose = Console.ReadLine();
+        
+        if (userChoose == "1")
+        {
+            PrimeNumber.FindNumber();
+        }
+        else if (userChoose == "2")
+        {
+            Chicken.Menu();
+        }
+        else
+        {
+            Console.WriteLine("Введите 1 или 2");
+        }
+    }
+}
+
